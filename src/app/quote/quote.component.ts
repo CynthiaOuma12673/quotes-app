7ae6cbc6f2d0c938aadce0f03b09bb1859ae8781
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-04 15:28:01 +0300

    add the constructors in the quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes:Quote[] = [
+    new Quote (1, 'Cynthia', 'Love', 'I love you for all that you are, all that you have been and all that you will be', new Date(2022,2,3), 0, 0),
+    new Quote (2, 'Leone', 'Technology', 'Technology is the best when it brings people together', new Date(2022,2,1), 0, 0),
+    new Quote (3, 'Lawrence', 'Motivational', 'The greatest glory in living lies not in never falling but in rising every time we fail', new Date(2022,1,31), 0, 0),
+    new Quote (4, 'Namenge', 'Time', 'Time is limited so do not waste it living someone elses life ', new Date(2022,2,4), 0, 0)
+  ];
 
   constructor() { }
 
