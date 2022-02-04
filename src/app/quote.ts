90c618d343aab767cc86b06e23db0df8836ab9f1
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-04 15:04:10 +0300

    it will change quote title spelling

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription:boolean;
-    constructor(public id:number, public name:string, public qoutetitle:string, public quote:string, public quoteDate:Date, public upCount:number, public:downCount:number)
+    constructor(public id:number, public name:string, public quotetitle:string, public quote:string, public quoteDate:Date, public upCount:number, public:downCount:number)
     {
         this.showDescription=false
     }
