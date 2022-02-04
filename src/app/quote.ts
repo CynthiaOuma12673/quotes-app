faaadaa6432c56ff20b8de0751fcacc71c8ffbaf
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-04 14:15:09 +0300

    it will create a constructor with the compnents of the quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+    showDescription:boolean;
+    constructor(public id:number, public name:string, public qoutetitle:string, public quote:string, public quoteDate:Date, public upCount:number, public:downCount:number)
+    {
+        this.showDescription=false
+    }
 }
