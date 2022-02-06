8353e96efb2ef143f5b86eac3da5884b2fc46129
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-06 09:32:02 +0300

    it will change the quote to quoteBody

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     showDescription:boolean;
-    constructor(public id:number, public name:string, public quotetitle:string, public quote:string, public quoteDate:Date, public upCount:number, public downCount:number)
+    constructor(public id:number, public name:string, public quotetitle:string, public quoteBody:string, public quoteDate:Date, public upCount:number, public downCount:number)
     {
-        this.showDescription=false
+        this.showDescription=false;
     }
 }
